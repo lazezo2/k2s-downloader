9eb02f4fb73bd2216beb779cc720ffad777f6e26
lazezo2 <81711582+lazezo2@users.noreply.github.com>
2024-02-03 01:49:30 +0300

    Update k2s.py
    disable img show!

--- k2s.py
+++ k2s.py
@@ -36,7 +36,8 @@ def generate_from_key(url: str, key: str, proxy: str) -> str:
             return r['url']
 
 def generate_download_urls(file_id: str, count: int = 1, skip: int = 0) -> list:
-
+    
+    
     if skip > 0:
         proxy_urls = get_working_proxies()[skip:]
     else:
@@ -46,8 +47,36 @@ def generate_download_urls(file_id: str, count: int = 1, skip: int = 0) -> list:
     urls = []
     captcha = requests.post(f"https://{choice(DOMAINS)}/api/v2/requestCaptcha").json()
     r = requests.get(captcha["captcha_url"])
+    
+    """
+    # in windows
     im = Image.open(BytesIO(r.content))
     im.show()
+    
+    # Save the image to a file
+    #captcha_image_path = 'captcha_image.png'
+    #im.save(captcha_image_path)
+    """
+    
+    # in google colab
+    
+    # Assuming r.content contains the image data
+    image_data = r.content
+
+    # Save the image directly from the content to a file
+    captcha_image_path = 'captcha_image.png'
+    with open(captcha_image_path, "wb") as file:
+        file.write(image_data)  
+    
+    """
+    # not work in colab!!
+    # Display the image using Colab's tools
+    from IPython.display import Image as IPImage, display
+
+    display(IPImage(filename=captcha_image_path))
+    # end of " in google colab"
+    """
+    
     response = input(f"Enter captcha response: ")
 
     for url in proxy_urls:
@@ -70,9 +99,24 @@ def generate_download_urls(file_id: str, count: int = 1, skip: int = 0) -> list:
             if free_r['status'] == "error":
                 if free_r["message"] == "Invalid captcha code":
                     r = requests.get(captcha["captcha_url"])
+                    
+                    """
+                    # in windows
                     im = Image.open(BytesIO(r.content))
                     im.show()
-                    response = input(f"Enter captcha response: ")
+                    """
+                    
+                    # in google colab
+    
+                    # Assuming r.content contains the image data
+                    image_data = r.content
+
+                    # Save the image directly from the content to a file
+                    captcha_image_path = 'captcha_image.png'
+                    with open(captcha_image_path, "wb") as file:
+                        file.write(image_data)  
+        
+                    response = input(f"Enter captcha response (2): ")
                     continue
                 elif free_r["message"] == "File not found":
                     sys.exit("File not found")
@@ -118,8 +162,30 @@ def generate_download_urls(file_id: str, count: int = 1, skip: int = 0) -> list:
 
     if not working_link:
         raise Exception("No working links found")
-
+    """
+    # Save the URLs to a file
+    save_file_path = "downloaded_urls.json"
+    with open(save_file_path, 'w') as file:
+        json.dump(urls, file)
+    save_file_path1 = "downloaded_urls1.json"
+    with open(save_file_path1, 'w') as file:
+        json.dump(urls[:count], file)
+    """
+    
+    """
+    # read urls from file
+    import json
+    urls = []
+    file_path = "downloaded_urls.json"
+    def load_download_urls(file_path: str = 'downloaded_urls.json') -> list:
+      with open(file_path, 'r') as file:
+        urls = json.load(file)
+      #print(urls)
+      return urls
+    urls = load_download_urls(file_path)
+    """
     return urls[:count]
+    
 
 def get_name(file_id: str) -> str:
     r = requests.post(f"https://{choice(DOMAINS)}/api/v2/getFilesInfo", json={
